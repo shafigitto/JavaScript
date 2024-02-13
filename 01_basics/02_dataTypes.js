2f8ad264dabee87502794010b1ff0dcc8d10d7aa
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-02-13 16:15:02 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "Shafi"
+let age = 23
+let isLoggedIn = true
+let city;
+
+
+/* 
+number => 2 to the power 53
+bigint => for large numbers
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value is not assigned like in city
+symbol = unique
+
+object
+*/
+
+console.log(typeof "Shafi")     // string
+console.log(typeof age)         // number
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
