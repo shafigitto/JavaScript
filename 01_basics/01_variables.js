21bd23285fcbe106e7ebcc6ec6aca3e54a743c58
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-02-13 15:46:10 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,12 +6,18 @@ accountCity = "Hyderabad"
 
 let accountState;
 
+
+
 /* 
     Prefer not to use var
     because of issues with block scope and functional scope
 */
 
 // accountId = 2 // not allowed
+
+accountEmail = "shufey@miscrosoft.com"
+accountPassword = "11111"
+accountCity = "Bangalore"
 
 console.log(accountId)
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
