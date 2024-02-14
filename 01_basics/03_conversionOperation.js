ee15b835c1c9578ff67298d8e9c385efd6844aef
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-02-14 10:37:13 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,18 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// let str1 = "Hello"
+// let str2 = " Shafi" 
+// let str3 = str1 + str2
+// console.log(str3)   // Hello Shafi
+
+
+// console.log("1" + 2)       // 12
+// console.log(1 + "2")       // 12
+// console.log("1" + 2 + 2)   // 122
+// console.log(1 + 2 + "2")   // 32
