cbcdf9f946fa53e58bf030aecab02b0c5343a68d
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-02-15 09:31:40 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "Shafi"
+const repoCount = 2
+
+// console.log(name + repoCount + "value")
+
+console.log(`My name is ${name} and I have ${repoCount} repositories in my Github.`)
+
+const myName = new String("Shafiuddin")
+
+console.log(myName[0])
+console.log(myName.length)
+console.log(myName.__proto__)
+console.log(myName.toUpperCase())
+console.log(myName.charAt(3))
+console.log(myName.indexOf('f'))
+
+
+const newString = myName.substring(0, 5)
+console.log(newString)
