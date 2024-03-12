48e70a48b2ce2cc2117f6947075c55b049390764
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-03-12 01:59:40 +0000

    Arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,35 @@
+const heroes = ["Shahrukh", "Shushant", "Rishi", "Vishu", "Shafi"]
+const heroines = ["Aishwarya", "Priyanka", "Kajol", "Preity zinta"]
+
+// heroes.push(heroines)
+
+// console.log(heroes)
+// console.log(heroes[5][1])
+
+// heroes.concat(heroines)
+// console.log(heroes)
+
+// const bollywood = heroes.concat(heroines)
+// console.log(bollywood)
+
+// const all = [...heroes, ...heroines]
+// console.log(all)        // Spread
+
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [8, [9, 10]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+
+// console.log(Array.isArray("Shafi"))
+// console.log(Array.from("Shafi"))
+// console.log(Array.from({ name: "Shafi" }))
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3))
