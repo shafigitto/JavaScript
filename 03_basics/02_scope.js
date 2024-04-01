c2c7980dc6b796c26931f834713c16bdaaa2e367
shafigitto <159920515+shafigitto@users.noreply.github.com>
2024-04-01 09:29:26 +0000

    Scopes and Hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,10 +5,55 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner : ", a);
+    // console.log("Inner : ", a); 
 }
 
 
 console.log(a);
 // console.log(b)
 // console.log(c);
+
+function one(){
+    const name = "shafi"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+    }
+
+    // console.log(website)
+
+    two()
+}
+
+one()
+
+
+if(true){
+    const username = "shafi"
+
+    if(username === "shafi"){
+        const website = " google"
+        console.log(username + website)
+    }
+
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+
+// ++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++
+
+addOne(5)
+
+function addOne(num){
+    return num + 1
+}
+
+addTwo(4)
+
+const addTwo = function (num){
+    return num + 2
+}
